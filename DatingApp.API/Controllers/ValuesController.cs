ce55a03cccd8a152573442c27a4cf7f0e3d76b17
unknown <amoltiwari777@gmail.com>
2020-04-11 21:24:51 +0530

    first commit

---
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -0,0 +1,55 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using DatingApp.API.Data;
+using System.Linq;
+
+
+namespace DatingApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+
+        private readonly DataContext _context;
+        public ValuesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public IActionResult GetValues()
+        {
+            var values = _context.Values.ToList();
+            return Ok(values);
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public IActionResult GetValues(int id)
+        {
+            var values = _context.Values.FirstOrDefault(x=>x.ID == id);
+            return Ok(values);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
